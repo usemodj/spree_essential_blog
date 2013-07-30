9cac11a581a3a754ebdbcd6405cae438367af250
Seo Seokjin <usemodj@gmail.com>
2013-07-30 21:38:39 +0900

    modified:   app/overrides/spree_essential_blog.rb 	modified:   config/routes.rb

--- app/overrides/spree_essential_blog.rb
+++ app/overrides/spree_essential_blog.rb
@@ -15,3 +15,10 @@ Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :insert_bottom => "#main-nav-bar",
                      :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/blog') ? 'current' : 'not'%>'><%= link_to t('blog'), '/blog'  %></li>",
                      :original => '442eefbb91b46a8773ce1de620f8f0a88a66caf1')
+
+# Deface::Override.new(:virtual_path => "spree/layouts/admin",
+#                      :name => "spree_essential_blog_admin_tab",
+#                      :insert_bottom => "[data-hook='admin_tabs']",
+#                      :text => "<%= tab(:posts, :icon => 'icon-file') %>",
+#                      :original => '172a4c6e9823af5cd0a87a452eadd9e369f36174'
+#                      )
